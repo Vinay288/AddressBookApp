a2411d459bcdc93a8b7e8d41e839c871b5e90e40
Vinay288 <vinayhiremath288@gmail.com>
2021-10-19 22:10:31 +0530

    MODIFIED: address regex pattern

--- js/utility.js
+++ js/utility.js
@@ -1,7 +1,7 @@
 let regex = {
     nameRegex: RegExp('^[A-Z]{1}[A-Za-z]{2,}([\\s]?([a-zA-Z]{3,}))*$'),
-    addressRegex: RegExp('^[a-zA-Z]{3,}([\\s]?[a-zA-Z]{3,})*$'),
-    phoneNumberRegex: RegExp('^[+]?[0-9]{2}[789]{1}[0-9]{9}$'),
+    addressRegex: RegExp('^[a-zA-Z0-9]{3,}([\\s]?[a-zA-Z0-9]{3,})*$'),
+    phoneNumberRegex: RegExp('^[+]?([0-9]{2})?[789]{1}[0-9]{9}$'),
     zipCodeRegex:RegExp('^[0-9]{3}\\s?[0-9]{3}$')
 };
 const checkName = (name) => {
@@ -21,7 +21,7 @@ const checkAddress = (address) => {
 }
 
 const checkPhoneNumber= (phoneNumber) => {
-    if (!regex.phoneNumberRegex.test(address)) {
+    if (!regex.phoneNumberRegex.test(phoneNumber)) {
         throw "Phone Number is Incorrect";
     }
     else
