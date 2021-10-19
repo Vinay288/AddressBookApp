6190b6c05521770a0419d004fc6c691cad9b7797
Vinay288 <vinayhiremath288@gmail.com>
2021-10-19 19:44:50 +0530

    ADDED: validation method for phone number

--- js/utility.js
+++ js/utility.js
@@ -1,7 +1,27 @@
+let regex = {
+    nameRegex: RegExp('^[A-Z]{1}[A-Za-z]{2,}([\s]?[a-zA-Z]{3,})*$'),
+    addressRegex: RegExp('^[a-zA-Z]{3,}([\s]?[a-zA-Z]{3,})*$'),
+    phoneNumberRegex: RegExp('^[+]?[0-9]{2}[789]{1}[0-9]{9}$')
+};
 const checkName = (name) => {
-    let nameRegex = RegExp('^[A-Z]{1}[A-Za-z]{2,}([\s]?[a-zA-Z]{3,})*$');
-    if (!nameRegex.test(name)) {
+    if (!regex.nameRegex.test(name)) {
         throw "Name is Incorrect";
+    }
+    else
+        return true;
+}
+
+const checkAddress = (address) => {
+    if (!regex.addressRegex.test(address)) {
+        throw "Address is Incorrect";
+    }
+    else
+        return true;
+}
+
+const checkPhoneNumber= (phoneNumber) => {
+    if (!regex.phoneNumberRegex.test(address)) {
+        throw "Phone Number is Incorrect";
     }
     else
         return true;
