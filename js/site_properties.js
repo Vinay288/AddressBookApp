22c01f7a0e8f01b576414f19c2d3fd18f589eeb2
Vinay288 <vinayhiremath288@gmail.com>
2021-10-20 23:15:13 +0530

    ADDED:uri for webpages

---
+++ js/site_properties.js
@@ -0,0 +1,6 @@
+let site_properties = {
+    localStorage: "true",
+    home_page: "../pages/address_book_home.html",
+    add_contacts: "../pages/address_book_form.html",
+    site_url: "http://127.0.0.1:3000/EmployeePayrollDB/"
+}
