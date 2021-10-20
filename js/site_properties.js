d857204198a62e8318116c6f82e06630f0928b94
Vinay288 <vinayhiremath288@gmail.com>
2021-10-21 01:21:40 +0530

    MODIFIED: site url

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,6 +1,6 @@
 let site_properties = {
-    localStorage: "true",
+    localStorage: "false",
     home_page: "../pages/address_book_home.html",
     add_contacts: "../pages/address_book_form.html",
-    site_url: "http://127.0.0.1:3000/EmployeePayrollDB/"
+    site_url: "http://127.0.0.1:3000/AddressBookDB/"
 }
