1133b96fabbd4ebd23ddc7a4995cb8964806bf94
Vinay288 <vinayhiremath288@gmail.com>
2021-10-20 23:14:41 +0530

    function to populate table using local storage

---
+++ js/address_book_home.js
@@ -0,0 +1,48 @@
+let contactsList;
+window.addEventListener("DOMContentLoaded", (event) => {
+    contactsList = getContactsDataFromStorage();
+    document.querySelector(".address-count").textContent = contactsList.length;
+    createInnerHtml();
+    localStorage.removeItem("editEmp");
+  });
+  const getContactsDataFromStorage = () => {
+    return localStorage.getItem("ContactsList")
+      ? JSON.parse(localStorage.getItem("ContactsList"))
+      : [];
+  };
+  
+  const createInnerHtml = () => {
+      console.log(contactsList);
+    if (contactsList.length == 0) return;
+    const headerHtml = "<tr><th>Full Name</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th>Actions</th></tr>";
+    let innerHtml = `${headerHtml}`;
+    for (const personData of contactsList) {
+      innerHtml = `${innerHtml}
+              <tr>
+                <td>${personData.name}</td>
+                <td>${personData.address}</td>
+                <td>${personData.city}</td>
+                <td>${personData.state}</td>
+                <td>${personData.zipcode}</td>
+                <td>${personData.phone}</td>
+                <td>
+                  <img
+                    id="${personData.id}"
+                    onclick="remove(this)"
+                    alt="delete"
+                    src="../assets/icons/delete-black-18dp.svg"
+                  />
+                  <img
+                    id="${personData.id}"
+                    alt="edit"
+                    onclick="update(this)"
+                    src="../assets/icons/create-black-18dp.svg"
+                  />
+                </td>
+              </tr>
+        `;
+      length += 1;
+    }
+    document.querySelector("#table-display").innerHTML = innerHtml;
+  };
+ 
