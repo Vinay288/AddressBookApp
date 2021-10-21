a5420f72304df8499d6515171a1883dd3a7a1f4d
Vinay288 <vinayhiremath288@gmail.com>
2021-10-21 08:49:11 +0530

    ADDED:url for state json file

--- js/site_properties.js
+++ js/site_properties.js
@@ -2,5 +2,6 @@ let site_properties = {
     localStorage: "false",
     home_page: "../pages/address_book_home.html",
     add_contacts: "../pages/address_book_form.html",
-    site_url: "http://127.0.0.1:3000/AddressBookDB/"
+    site_url: "http://127.0.0.1:3000/AddressBookDB/",
+    state_url:"http://localhost:4000/State/"
 }
